f905920463f822db48c67a6fd175fc700823e148
Teerawat Wuttiwat <781147+twuttiwat@users.noreply.github.com>
2025-11-13 13:32:46 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,26 +1,36 @@
 import socket
+import threading
 
+def handle_client(client_socket):
+    try:
+        while True:
+            # Receive data from client
+            data = client_socket.recv(1024)
+            print(f"Message received: {data}")
+            if not data:
+                break
+
+            # Send response to client
+            client_socket.sendall(b"+PONG\r\n")
+
+    except (ConnectionResetError, BrokenPipeError):
+        pass
+    finally:
+        client_socket.close()
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
     # Listen to client
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
 
-    # Accept connection
-    client_socket, client_address = server_socket.accept()  # wait for client
-    print(f"Connection established with {client_address}")
-    
+   
     while True:
 
-        # Receive data from client
-        data = client_socket.recv(1024)
-        print(f"Message received: {data}")
+        # Accept connection
+        client_socket, _ = server_socket.accept()  # wait for client
+        print(f"Connection established")
 
-        # Send response to client
-        client_socket.sendall(b"+PONG\r\n")
-
+        # Handle client using new thread
+        threading.Thread(target=handle_client, args=(client_socket,)).start()
 
 if __name__ == "__main__":
     main()
