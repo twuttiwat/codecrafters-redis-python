b414046c8bf891da53069d0e38d2d7371be67e31
Teerawat Wuttiwat <781147+twuttiwat@users.noreply.github.com>
2025-11-13 10:46:01 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,7 +6,8 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept()  # wait for client
+    connection,_ = server_socket.accept()  # wait for client
+    connection.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
