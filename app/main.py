4e5adbcf6735cfe8ddbac248f29f37fdbb7cab41
Teerawat Wuttiwat <781147+twuttiwat@users.noreply.github.com>
2026-02-04 22:03:22 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.accept() # wait for client
+    server_socket.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
