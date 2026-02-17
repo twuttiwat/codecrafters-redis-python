c3757e117e5ab95b4184b8cbafaf8df1232f1561
Teerawat Wuttiwat <781147+twuttiwat@users.noreply.github.com>
2026-02-17 21:18:24 +0700

    codecrafters submit [skip ci]

--- app/state/ClientState.py
+++ app/state/ClientState.py
@@ -9,3 +9,18 @@ class ClientState:
     def queue_command(self, func, args):
         self.multi_queue.append((func, args))
         return resp.encode_simple_str("QUEUED")
+
+    def multi(self):
+        self.is_multi = True
+
+    async def exec(self):
+        if not self.is_multi:
+            raise ValueError("ERR EXEC without MULTI")
+
+        results = []
+        for func, args in self.multi_queue:
+            results.append(await func(*args))
+        self.is_multi = False
+        self.multi_queue = []
+
+        return results
